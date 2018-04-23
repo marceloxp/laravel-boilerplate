58c21b20ee2939084d35bf8d4e9441e5604abaab
marceloxp <marceloxp@gmail.com>
2018-04-23 10:37:02 -0300

    - Added migration to generateuniquecode and generaterandom functions (Table codes).

---
+++ database/migrations/2018_04_20_185316_create_codes.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCodes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create
+		(
+			'codes',
+			function(Blueprint $table)
+			{
+				$table->increments('id');
+				$table->string('code',8)->unique()->comment('Código');
+				$table->timestamps();
+				$table->softDeletes();
+        	}
+		);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('codes');
+    }
+}
