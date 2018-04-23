32d30b13556386f580559a8236881425a8db2a74
marceloxp <marceloxp@gmail.com>
2018-04-23 10:33:49 -0300

    - Added migration to generateuniquecode and generaterandom functions.

---
+++ database/migrations/2018_04_20_185516_create_function_generateuniquecode.php
@@ -0,0 +1,98 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFunctionGenerateuniquecode extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+		$prefix = trim(DB::getTablePrefix(), '_');
+
+		$querys = 
+		[
+			"
+				CREATE FUNCTION `" . $prefix . "_generateuniquecode`() RETURNS varchar(8)
+				BEGIN
+					DECLARE unqstr varchar(8);
+					SET unqstr = CONCAT
+					(
+						substring('abcdefghijklmnopqrstuvwxyz0123456789', rand()*36+1, 1),
+						substring('abcdefghijklmnopqrstuvwxyz0123456789', rand()*36+1, 1),
+						substring('abcdefghijklmnopqrstuvwxyz0123456789', rand()*36+1, 1),
+						substring('abcdefghijklmnopqrstuvwxyz0123456789', rand()*36+1, 1),
+						substring('abcdefghijklmnopqrstuvwxyz0123456789', rand()*36+1, 1),
+						substring('abcdefghijklmnopqrstuvwxyz0123456789', rand()*36+1, 1),
+						substring('abcdefghijklmnopqrstuvwxyz0123456789', rand()*36+1, 1),
+						substring('abcdefghijklmnopqrstuvwxyz0123456789', rand()*36+1, 1) 
+					);
+					WHILE EXISTS(SELECT id FROM `unc_codes` WHERE code = unqstr) DO
+						SET unqstr = CONCAT
+						(
+							substring('abcdefghijklmnopqrstuvwxyz0123456789', rand()*36+1, 1),
+							substring('abcdefghijklmnopqrstuvwxyz0123456789', rand()*36+1, 1),
+							substring('abcdefghijklmnopqrstuvwxyz0123456789', rand()*36+1, 1),
+							substring('abcdefghijklmnopqrstuvwxyz0123456789', rand()*36+1, 1),
+							substring('abcdefghijklmnopqrstuvwxyz0123456789', rand()*36+1, 1),
+							substring('abcdefghijklmnopqrstuvwxyz0123456789', rand()*36+1, 1),
+							substring('abcdefghijklmnopqrstuvwxyz0123456789', rand()*36+1, 1),
+							substring('abcdefghijklmnopqrstuvwxyz0123456789', rand()*36+1, 1) 
+						);
+					END WHILE;
+					INSERT INTO `unc_codes`(code, created) VALUES (unqstr, now());
+					RETURN unqstr;
+				END;
+			",
+			"
+				CREATE FUNCTION `" . $prefix . "_generaterandom`() RETURNS varchar(8)
+				BEGIN
+					DECLARE unqstr varchar(8);
+					SET unqstr = CONCAT
+					(
+						substring('abcdefghijklmnopqrstuvwxyz0123456789', rand()*36+1, 1),
+						substring('abcdefghijklmnopqrstuvwxyz0123456789', rand()*36+1, 1),
+						substring('abcdefghijklmnopqrstuvwxyz0123456789', rand()*36+1, 1),
+						substring('abcdefghijklmnopqrstuvwxyz0123456789', rand()*36+1, 1),
+						substring('abcdefghijklmnopqrstuvwxyz0123456789', rand()*36+1, 1),
+						substring('abcdefghijklmnopqrstuvwxyz0123456789', rand()*36+1, 1),
+						substring('abcdefghijklmnopqrstuvwxyz0123456789', rand()*36+1, 1),
+						substring('abcdefghijklmnopqrstuvwxyz0123456789', rand()*36+1, 1) 
+					);
+					RETURN unqstr;
+				END;
+			"
+		];
+
+		foreach ($querys as $query)
+		{
+			DB::unprepared($query);
+		}
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+		$prefix = trim(DB::getTablePrefix(), '_');
+
+		$querys = 
+		[
+			"DROP FUNCTION IF EXISTS `" . $prefix . "_generateuniquecode`;",
+			"DROP FUNCTION IF EXISTS `" . $prefix . "_generaterandom`;"
+		];
+
+		foreach ($querys as $query)
+		{
+			DB::unprepared($query);
+		}
+    }
+}
