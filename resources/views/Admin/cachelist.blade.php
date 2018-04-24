be5661a1feb7375fa60bd9f8d0cb44b7419b4e11
marceloxp <marceloxp@gmail.com>
2018-04-24 15:07:22 -0300

    - Better cache list.

--- resources/views/Admin/cachelist.blade.php
+++ resources/views/Admin/cachelist.blade.php
@@ -11,7 +11,18 @@
 @section('content')
 	<div class="row">
 		<div class="col-md-12">
-			{{ r($caches) }}
+			@foreach ($caches as $prefix => $cache)
+				<h4>{{ $prefix }}</h4>
+				<pre><code class="language-json line-numbers">{{ json_encode($cache, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE) }}</code></pre>
+			@endforeach
 		</div>
 	</div>
+@endsection
+
+@section('styles')
+	{!! css('/admin-lte-custom/css/prism.css') !!}
+@endsection
+
+@section('scripts')
+	{!! script('/admin-lte-custom/js/prism.js') !!}
 @endsection
