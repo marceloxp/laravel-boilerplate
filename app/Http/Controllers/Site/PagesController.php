18ea8d32eb154f3c6ae379d4a1f6b83b89bcb907
Marcelo de Souza Lima <marceloxp@gmail.com>
2018-11-19 18:34:03 -0200

    - Cart.

--- app/Http/Controllers/Site/PagesController.php
+++ app/Http/Controllers/Site/PagesController.php
@@ -6,7 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\View;
 use App\Http\Utilities\Datasite;
 use App\Http\Utilities\MetaSocial;
-use Faker\Factory as Faker;
+use App\Http\Utilities\Pocket;
+use App\Http\Utilities\ProductValue;
 
 class PagesController extends Controller
 {
@@ -19,6 +20,46 @@ class PagesController extends Controller
 	{
 		MetaSocial::append('title', ' - Fale Conosco');
 		MetaSocial::set('description', 'Entre em contato conosco.');
+
+		$product = (Object)
+		[
+			'id'       => 152,
+			'name'     => 'Product 1',
+			'price'    => 10,
+			'discount' => 50
+		];
+
+		$price = new ProductValue(10, 1, 50);
+		$price->setPayment('visa', 0, 3);
+		$price->setPayment('incash', 7, 1);
+
+		$product->value = $price;
+
+		r($product);
+
+		~r('Done');
+
+		// $pv = new ProductValue(10, 1, 50);
+		// $pv->setPayment('visa', 0, 12);
+		// $pv->setPayment('incash', 7, 1);
+		// ~r($pv);
+
+		// $payments = $pv->getPayment('visa');
+		// ~r($payments);
+
+		// Pocket::reset();
+
+		// Pocket::del('product_1');
+		// Pocket::del('product_2');
+
+		// $product_1 = Pocket::set('Livro 1', 10, 1);
+		// r($product_1);
+
+		// $product_2 = Pocket::set('Livro 2', 10, 1);
+		// r($product_2);
+
+		// $all = Pocket::all();
+		// r($all);
 		
 		return view('site/pages/faleconosco');
 	}
---
+++ app/Http/Utilities/Cart.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Utilities;
+
+use Session;
+
+class Cart
+{
+	const name = 'cart';
+	const prefix = 'cart.';
+
+	private static function getName($p_id)
+	{
+		return starts_with($p_id, self::prefix) ? $p_id : sprintf(self::prefix . $p_id);
+	}
+
+	public static function get($p_id)
+	{
+		$name = self::getName($p_id);
+		return \Session::get($name) ? 1;
+	}
+
+	public static function add($p_id, $p_quant = 1)
+	{
+		$name = self::getName($p_id);
+		$quant = self::get($name);
+		$quant += $p_quant;
+		\Session::put($name, $quant);
+		return $value;
+	}
+
+	public static function inc($p_id, $p_quant = 1)
+	{
+		return self::add($p_id, $p_quant = 1);
+	}
+
+	public static function del($p_id)
+	{
+		$name = self::getName($p_id);
+		\Session::forget($name);
+	}
+
+	public static function reset()
+	{
+		\Session::forget(self::name);
+	}
+}
