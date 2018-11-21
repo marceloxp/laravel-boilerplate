f1e47fd1c912f5e9f121931a7db34a1260e036b8
Marcelo de Souza Lima <marceloxp@gmail.com>
2018-11-21 17:59:58 -0200

    - Remove unused uses.

--- app/Http/Controllers/Site/PagesController.php
+++ app/Http/Controllers/Site/PagesController.php
@@ -4,12 +4,8 @@ namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\View;
-use App\Http\Utilities\Cart;
-use App\Http\Utilities\ProductCart;
 use App\Http\Utilities\Datasite;
 use App\Http\Utilities\MetaSocial;
-use App\Http\Utilities\Pocket;
-use App\Http\Utilities\ProductValue;
 
 class PagesController extends Controller
 {
