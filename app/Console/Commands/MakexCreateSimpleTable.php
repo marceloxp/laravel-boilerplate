d7f5434e32754aa7afc7af4de90a44fcc6f20937
Marcelo de Souza Lima <marceloxp@gmail.com>
2019-01-30 17:13:17 -0200

    - Ajust console command title.

--- app/Console/Commands/MakexCreateSimpleTable.php
+++ app/Console/Commands/MakexCreateSimpleTable.php
@@ -105,8 +105,13 @@ class Create{ClassName}Table extends Migration
 	{
 		$this->clear();
 
-		$this->info('CREATE SIMPLE TABLE');
-		$this->br();
+		$this->info('');
+		$this->info('   __________  _________  ____________   _________    ____  __    ______');
+		$this->info('  / ____/ __ \/ ____/   |/_  __/ ____/  /_  __/   |  / __ )/ /   / ____/');
+		$this->info(' / /   / /_/ / __/ / /| | / / / __/      / / / /| | / __  / /   / __/   ');
+		$this->info('/ /___/ _, _/ /___/ ___ |/ / / /___     / / / ___ |/ /_/ / /___/ /___   ');
+		$this->info('\____/_/ |_/_____/_/  |_/_/ /_____/    /_/ /_/  |_/_____/_____/_____/   ');
+		$this->info('');
 
 		$folder_name = $this->ask('Folder name (ex: Models)', 'Models');
 		$folder_name = (empty($folder_name)) ? 'Models' : $folder_name;
