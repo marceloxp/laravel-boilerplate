332e335e18847bd6f9a535170d9158f0cde25219
Marcelo XP <marceloxp@gmail.com>
2019-03-01 11:42:12 -0300

    - ajust users migration file name.

--- database/migrations/2014_01_01_120000_create_users_table.php
+++ database/migrations/2014_01_01_120000_create_users_table.php
@@ -3,7 +3,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AjustUsersTable extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
