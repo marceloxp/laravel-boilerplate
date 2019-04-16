78e9ab1b27660fdbfe1aaa0fa973d1c35b17bc9e
Marcelo XP <marceloxp@gmail.com>
2019-04-16 16:53:27 -0300

    - Code fix.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,21 +4,21 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
 		$this->call(ConfigsTableSeeder::class);
 		$this->call(RolesTableSeeder::class);
 		$this->call(UsersTableSeeder::class);
 		$this->call(CategoriesTableSeeder::class);
-        $this->call(MenusTableSeeder::class);
+		$this->call(MenusTableSeeder::class);
 		$this->call(VideosTableSeeder::class);
-        $this->call(StatesTableSeeder::class);
+		$this->call(StatesTableSeeder::class);
 		$this->call(CitiesTableSeeder::class);
-        $this->call(TagsTableSeeder::class);
-    }
+		$this->call(TagsTableSeeder::class);
+	}
 }
